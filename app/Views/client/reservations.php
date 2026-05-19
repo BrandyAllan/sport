d01c01689f3551dfe8f0b951dd0868ae7955e039
BrandyAllan <brandyratsimbaharison46@gmail.com>
2026-05-19 16:32:02 +0300

    js local

--- app/Views/client/reservations.php
+++ app/Views/client/reservations.php
@@ -99,7 +99,7 @@
 </div>
 <link href="https://cdn.jsdelivr.net/npm/fullcalendar@6.1.15/index.global.min.css" rel="stylesheet">
 
-<script src="https://cdn.jsdelivr.net/npm/fullcalendar@6.1.15/index.global.min.js"></script>
+<script src="assets/js/index.global.min.js"></script>
 
 <script>
 document.addEventListener('DOMContentLoaded', function () {
